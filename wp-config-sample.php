404eaf173454a17df5021b0b723855345fd954bc
V469 <65vas.vs@gmail.com>
2025-04-11 13:33:14 +0300

    docs: Add wp-config-sample.php as a secure configuration example

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,102 +1,60 @@
-<?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the website, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
- *
- * @package WordPress
- */
-
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
-
-/** Database username */
-define( 'DB_USER', 'username_here' );
-
-/** Database password */
-define( 'DB_PASSWORD', 'password_here' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
-
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- *
- * At the installation time, database tables are created with the specified prefix.
- * Changing this value after WordPress is installed will make your site think
- * it has not been installed.
- *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
- */
-$table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
- */
-define( 'WP_DEBUG', false );
-
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
-}
-
-/** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
+<?php
+/**
+ * Базова конфігурація WordPress.
+ * Цей файл є прикладом налаштувань. Скопіюйте його як wp-config.php
+ * і заповніть своїми даними.
+ */
+
+// ** Налаштування MySQL ** //
+/** Ім'я бази даних для WordPress */
+define( 'DB_NAME', 'your_database_name' );
+
+/** Ім'я користувача MySQL */
+define( 'DB_USER', 'your_database_user' );
+
+/** Пароль до бази даних MySQL */
+define( 'DB_PASSWORD', 'your_database_password' );
+
+/** Адреса сервера MySQL */
+define( 'DB_HOST', 'localhost' );
+
+/** Кодування бази даних */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Схема порівняння */
+define( 'DB_COLLATE', '' );
+
+/**
+ * Унікальні ключі та солі автентифікації.
+ * Згенеруйте їх на https://api.wordpress.org/secret-key/1.1/salt/
+ */
+define( 'AUTH_KEY',         'впишіть_свій_унікальний_ключ' );
+define( 'SECURE_AUTH_KEY',  'впишіть_свій_унікальний_ключ' );
+define( 'LOGGED_IN_KEY',    'впишіть_свій_унікальний_ключ' );
+define( 'NONCE_KEY',        'впишіть_свій_унікальний_ключ' );
+define( 'AUTH_SALT',        'впишіть_свій_унікальний_ключ' );
+define( 'SECURE_AUTH_SALT', 'впишіть_свій_унікальний_ключ' );
+define( 'LOGGED_IN_SALT',   'впишіть_свій_унікальний_ключ' );
+define( 'NONCE_SALT',       'впишіть_свій_унікальний_ключ' );
+
+/**
+ * Префікс таблиць для бази даних WordPress.
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Налаштування режиму налагодження.
+ * Рекомендується вимкнути для продакшену.
+ */
+define( 'WP_DEBUG', false );
+
+/* Це все, припиніть редагування! Успішного блогування. */
+
+/** Абсолютний шлях до директорії WordPress. */
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Інсталяція WordPress */
+require_once ABSPATH . 'wp-settings.php';
+
